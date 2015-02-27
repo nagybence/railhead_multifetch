28e2e2fc22fc21c9c4cb7fba417c340ba392d0ba
Nagy Bence <nagybence@tipogral.hu>
2015-02-27 09:41:31 +0100

    initial import

---
+++ lib/railhead_multifetch.rb
@@ -0,0 +1,41 @@
+require 'action_view/digestor'
+
+
+module RailheadMultifetch
+
+  def self.included(base)
+    base.class_eval do
+      alias_method_chain :render_collection, :multifetch
+    end
+  end
+
+  def render_collection_with_multifetch
+    if ActionController::Base.perform_caching && (@options[:cache].present? or @locals[:cache].present?)
+      return nil if @collection.blank?
+      results = []
+      keymap = {}
+      digest = ActionView::PartialDigestor.new(name: @template.virtual_path, finder: @view.lookup_context).digest
+
+      @collection.each do |object|
+        item = @options[:cache].respond_to?(:call) ? @options[:cache].call(object) : object
+        key = @view.controller.fragment_cache_key([item, digest])
+        keymap[key] = item
+      end
+      mutable_keys = keymap.keys.map { |key| key.dup }
+      cached_results = Rails.cache.read_multi(*mutable_keys)
+      @collection = (keymap.keys - cached_results.keys).map { |key| keymap[key] }
+      non_cached_results = @collection.present? ? collection_with_template : []
+      mutable_keys.each { |key| results << (cached_results[key] || non_cached_results.shift) }
+
+      if @options.key?(:spacer_template)
+        spacer = find_template(@options[:spacer_template]).render(@view, @locals)
+      end
+      results.join(spacer).html_safe
+    else
+      render_collection_without_multifetch
+    end
+  end
+end
+
+
+ActionView::PartialRenderer.send :include, RailheadMultifetch
---
+++ railhead_multifetch.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name     = "railhead_multifetch"
+  s.version  = "0.0.1"
+  s.date     = "2015-02-27"
+  s.summary  = "RailheadMultifetch is a Ruby on Rails plugin that automatically loads multiple cached partials."
+  s.email    = "bence.nagy@gmail.com"
+  s.homepage = "http://github.com/nagybence/railhead_multifetch"
+  s.has_rdoc = true
+  s.authors  = ["Bence Nagy"]
+  s.files    = ["MIT-LICENSE",
+                "README.rdoc",
+                "railhead_multifetch.gemspec",
+                "lib/railhead_multifetch.rb"]
+  s.rdoc_options = ["--main", "README.rdoc"]
+  s.license = "MIT"
+end
